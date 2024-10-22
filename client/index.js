ec36a8f55462a77e4aebd9a92d4ef033b6f4e59f
Kyle Harding <kyle@balena.io>
2024-10-22 13:31:32 -0400

    Add support for backend groups
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- client/index.js
+++ client/index.js
@@ -1,19 +1,23 @@
 const Docker = require('dockerode');
 
-// Function to generate a unique client ID
-function generateClientId() {
+function generateBuildId() {
   return Math.random().toString(36).substring(7);
 }
 
-let clientId = generateClientId();
+function changeGroup(currentGroup) {
+  return currentGroup === 'A' ? 'B' : 'A';
+}
+
+let buildId = generateBuildId();
+let buildGroup = 'A';
 let requestCount = 0;
 
-// Function to create a new Docker client with the current client ID
 function createDockerClient() {
   return new Docker({
     host: process.env.DOCKER_HOST || 'http://localhost:2375',
     headers: {
-      'X-Docker-Client-ID': clientId
+      'X-Balena-Build-ID': buildId,
+      'X-Balena-Build-Group': buildGroup
     }
   });
 }
@@ -23,31 +27,36 @@ let docker = createDockerClient();
 async function getDockerInfo() {
   try {
     const info = await docker.info();
+    
     console.log(`Request #: ${requestCount + 1}`);
-    console.log(`Client ID: ${clientId}`);
-    console.log(`Docker Daemon ID: ${info.ID}`);
-    console.log(`Name: ${info.Name}`);
-    console.log(`Operating System: ${info.OperatingSystem}`);
-    console.log(`Kernel Version: ${info.KernelVersion}`);
+    console.log(`Build ID: ${buildId}`);
+    console.log(`Build Group: ${buildGroup}`);
+    console.log(`Docker Daemon Name: ${info.Name}`);
     console.log(`---`);
 
     requestCount++;
 
-    // Change client ID every 4 requests
+    // Change Build ID every 4 requests
     if (requestCount % 4 === 0) {
-      clientId = generateClientId();
-      console.log(`\nChanging to new Client ID: ${clientId}\n`);
+      buildId = generateBuildId();
+      // Also randomly change the group sometimes
+      if (Math.random() < 0.5) {
+        buildGroup = changeGroup(buildGroup);
+        console.log(`\nChanging to new Group: ${buildGroup}`);
+      }
+      console.log(`\nChanging to new Build ID: ${buildId}\n`);
       docker = createDockerClient();
     }
   } catch (error) {
     console.error('Error getting Docker info:', error);
+    console.error('Current headers:', docker.modem.headers);
   }
 }
 
 async function main() {
-  console.log(`Starting Docker client with initial ID: ${clientId}`);
-  
-  // Run getDockerInfo every 2 seconds
+  console.log(`Starting Docker client with initial Build ID: ${buildId} and Group: ${buildGroup}`);
+  // Add a delay before starting requests
+  await new Promise(resolve => setTimeout(resolve, 8000));
   setInterval(getDockerInfo, 2000);
 }
 
